16a4fd0b75f2f5fae9433f6a84c6d54788be1c71
KPRios <kim.rios@g.austincc.edu>
2025-05-18 04:15:07 +0000

    Question 5

--- src/main.cpp
+++ src/main.cpp
@@ -5,52 +5,54 @@
 #include "shooter.h"
 #include "come_out_phase.h"
 #include "point_phase.h"
-
-void test_come_out_phase() {
-    ComeOutPhase come_out;
-
-    int natural_vals[] = {7, 11};
-    int craps_vals[] = {2, 3, 12};
-    int point_vals[] = {4, 5, 6, 8, 9, 10};
-
-    for (int val : natural_vals) {
-        roll* r = new roll(val);  // test constructor: Roll(int value)
-        assert(come_out.get_outcome(r) == RollOutcome::natural);
-        delete r;
-    }
-
-    for (int val : craps_vals) {
-        roll* r = new roll(val);
-        assert(come_out.get_outcome(r) == RollOutcome::craps);
-        delete r;
-    }
-
-    for (int val : point_vals) {
-        roll* r = new roll(val);
-        assert(come_out.get_outcome(r) == RollOutcome::point);
-        delete r;
-    }
-}
-
-void test_point_phase() {
-    PointPhase phase(5);
-
-    roll* r1 = new roll(5);  // should be point
-    assert(phase.get_outcome(r1) == RollOutcome::point);
-    delete r1;
-
-    roll* r2 = new roll(7);  // should be seven_out
-    assert(phase.get_outcome(r2) == RollOutcome::seven_out);
-    delete r2;
-
-    roll* r3 = new roll(9);  // should be nopoint
-    assert(phase.get_outcome(r3) == RollOutcome::nopoint);
-    delete r3;
-}
+#include <cstdlib>     
+#include <ctime> 
 
 int main() {
-    test_come_out_phase();
-    test_point_phase();
-    std::cout << "All phase tests passed!" << std::endl;
+    srand(time(0)); 
+
+    die die1, die2;
+    shooter shooter;
+    roll* roll = shooter.throw_dice(die1, die2);
+
+    int rolled_value = roll->result();
+    ComeOutPhase come_out_phase;
+
+    while (true) {
+        RollOutcome outcome = come_out_phase.get_outcome(roll);
+
+        if (outcome == RollOutcome::natural || outcome == RollOutcome::craps) {
+            std::cout << "Rolled " << roll->result() << " roll again\n";
+            roll = shooter.throw_dice(die1, die2);
+        } else {
+            break;
+        }
+    }
+
+    rolled_value = roll->result();
+    std::cout << "Rolled " << rolled_value << " start of point phase\n";
+    std::cout << "Roll until " << rolled_value << " or a 7 is rolled\n";
+
+    int point = rolled_value;
+    roll = shooter.throw_dice(die1, die2);
+
+    PointPhase point_phase(point);
+
+    while (true) {
+        RollOutcome outcome = point_phase.get_outcome(roll);
+        rolled_value = roll->result();
+
+        if (outcome == RollOutcome::point || outcome == RollOutcome::nopoint) {
+            std::cout << "Rolled " << rolled_value << " roll again\n";
+            roll = shooter.throw_dice(die1, die2);
+        } else {
+            break;
+        }
+    }
+
+    std::cout << "Rolled " << roll->result() << " end of point phase\n\n";
+
+    shooter.display_rolled_values();
+
     return 0;
 }
